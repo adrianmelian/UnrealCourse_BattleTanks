0e8a7cf106e4ad18ff6aaf991f2eb786ee7d9a5a
amelian <adrianmelian123@gmail.com>
2019-11-18 22:10:52 -0800

    Starting the project

---
+++ BattleTanksAssets/Source/BattleTanksAssets/BattleTanksAssets.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTanksAssets.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BattleTanksAssets, "BattleTanksAssets" );
