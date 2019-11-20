05a5138522d8be57a556e07377f0fecd18afd5d8
amelian <adrianmelian123@gmail.com>
2019-11-20 15:29:24 -0800

    Start C++ Tank and TankPlayerController classes, extended from pawn and playerController classes. Changed Tank Blueprints to look at the new extended classes instead of base

---
+++ BattleTanksAssets/Source/BattleTanksAssets/Private/TankPlayerController.cpp
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TankPlayerController.h"
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("Begin Play called from TankPlayerController"));
+	ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController not possessing tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController is possessing %s"), *ControlledTank->GetName());
+	}
+}
+
+ATank* ATankPlayerController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
